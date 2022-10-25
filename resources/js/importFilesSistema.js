4a6960ed29751929713e50120ba6f9776c4a8b48
danielRojas14 <danielrojas131415@gmail.com>
2022-10-25 16:48:07 -0300

    se esta realizando la migracion del template

---
+++ resources/js/importFilesSistema.js
@@ -0,0 +1,68 @@
+// bootstrap css
+import "../../vendor/feather/feather.css";
+import "../../vendor/ti-icons/css/themify-icons.css";
+import "../../vendor/css/vendor.bundle.base.css";
+import "../../vendor/datatables.net-bs4/dataTables.bootstrap4.css";
+import "../../vendor/ti-icons/css/themify-icons.css";
+import "../css/select.dataTables.min.css";
+import "../css/vertical-layout-light/style.css";
+
+
+import $ from "jquery";
+window.$ = window.jQuery = $;
+
+
+
+import "../js/libs/vendor.bundle.base.js";
+
+import "../../vendor/datatables.net/jquery.dataTables.js";
+
+import "../../vendor/datatables.net-bs4/dataTables.bootstrap4.js";
+
+import "../js/libs/dataTables.select.min.js";
+
+import "../js/libs/off-canvas.js";
+
+import "../js/libs/hoverable-collapse.js";
+
+import "../js/libs/template.js";
+
+// import "../js/libs/settings.js";
+
+// import "../js/libs/todolist.js";
+
+
+
+
+
+
+
+
+// import "../css/main/main.css";
+// // ------------------------------------------------------------
+// import "./layoutToggle/toggleNavbar.js";
+
+
+
+
+// <!-- plugins:js -->
+{/* <script src="vendors/js/vendor.bundle.base.js"></script> */}
+// <!-- endinject -->
+// <!-- Plugin js for this page -->
+{/* <script src="vendors/chart.js/Chart.min.js"></script> */}
+{/* <script src="vendors/datatables.net/jquery.dataTables.js"></script> */}
+{/* <script src="vendors/datatables.net-bs4/dataTables.bootstrap4.js"></script> */}
+{/* <script src="js/dataTables.select.min.js"></script> */}
+
+// <!-- End plugin js for this page -->
+// // <!-- inject:js -->
+// <script src="js/off-canvas.js"></script>
+// <script src="js/hoverable-collapse.js"></script>
+// <script src="js/template.js"></script>
+// <script src="js/settings.js"></script>
+// <script src="js/todolist.js"></script>
+// <!-- endinject -->
+// <!-- Custom js for this page-->
+// <script src="js/dashboard.js"></script>
+// <script src="js/Chart.roundedBarCharts.js"></script>
+// <!-- End custom js for this page-->
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,284 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>@yield('titulo')</title>
+
+    <link rel="shortcut icon" href="{{ url('./img/favicon.png') }}" />
+
+    @vite(['resources/js/importFilesSistema.js'])
+
+
+
+
+</head>
+
+<body>
+
+
+    <div class="container-scroller">
+
+
+
+
+
+        {{-- NAVBAR --}}
+
+        <nav class="navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
+            <div class="text-center navbar-brand-wrapper d-flex align-items-center justify-content-center">
+                <a class="navbar-brand brand-logo mr-5" href="{{url('inicio')}}"><h3 class="mr-2" alt="logo"><b>INNOVA S.R.L</b></h3></a>
+                <a class="navbar-brand brand-logo-mini" href="{{url('inicio')}}"><h3 class="p-2" alt="logo">IN</h3></a>
+            </div>
+            <div class="navbar-menu-wrapper d-flex align-items-center justify-content-end">
+                <button class="navbar-toggler navbar-toggler align-self-center" type="button" data-toggle="minimize">
+                    <span class="icon-menu"></span>
+                </button>
+                <ul class="navbar-nav mr-lg-2">
+                    {{-- <li class="nav-item nav-search d-none d-lg-block">
+                        <div class="input-group">
+                            <div class="input-group-prepend hover-cursor" id="navbar-search-icon">
+                                <span class="input-group-text" id="search">
+                                    <i class="icon-search"></i>
+                                </span>
+                            </div>
+                            <input type="text" class="form-control" id="navbar-search-input" placeholder="Search now"
+                                aria-label="search" aria-describedby="search">
+                        </div>
+                    </li> --}}
+                </ul>
+                <ul class="navbar-nav navbar-nav-right">
+                    <li class="nav-item dropdown">
+                        <a class="nav-link count-indicator dropdown-toggle" id="notificationDropdown" href="#"
+                            data-toggle="dropdown">
+                            <i class="icon-bell mx-0"></i>
+                            <span class="count"></span>
+                        </a>
+                        <div class="dropdown-menu dropdown-menu-right navbar-dropdown preview-list"
+                            aria-labelledby="notificationDropdown">
+                            <p class="mb-0 font-weight-normal float-left dropdown-header">Notifications</p>
+                            <a class="dropdown-item preview-item">
+                                <div class="preview-thumbnail">
+                                    <div class="preview-icon bg-success">
+                                        <i class="ti-info-alt mx-0"></i>
+                                    </div>
+                                </div>
+                                <div class="preview-item-content">
+                                    <h6 class="preview-subject font-weight-normal">Application Error</h6>
+                                    <p class="font-weight-light small-text mb-0 text-muted">
+                                        Just now
+                                    </p>
+                                </div>
+                            </a>
+                            <a class="dropdown-item preview-item">
+                                <div class="preview-thumbnail">
+                                    <div class="preview-icon bg-warning">
+                                        <i class="ti-settings mx-0"></i>
+                                    </div>
+                                </div>
+                                <div class="preview-item-content">
+                                    <h6 class="preview-subject font-weight-normal">Settings</h6>
+                                    <p class="font-weight-light small-text mb-0 text-muted">
+                                        Private message
+                                    </p>
+                                </div>
+                            </a>
+                            <a class="dropdown-item preview-item">
+                                <div class="preview-thumbnail">
+                                    <div class="preview-icon bg-info">
+                                        <i class="ti-user mx-0"></i>
+                                    </div>
+                                </div>
+                                <div class="preview-item-content">
+                                    <h6 class="preview-subject font-weight-normal">New user registration</h6>
+                                    <p class="font-weight-light small-text mb-0 text-muted">
+                                        2 days ago
+                                    </p>
+                                </div>
+                            </a>
+                        </div>
+                    </li>
+                    <li class="nav-item nav-profile dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown" id="profileDropdown">
+                            NOMBRE-USUARIO
+                        </a>
+                        <div class="dropdown-menu dropdown-menu-right navbar-dropdown"
+                            aria-labelledby="profileDropdown">
+                            <a class="dropdown-item">
+                                <i class="ti-settings text-primary"></i>
+                                Settings
+                            </a>
+                            <a class="dropdown-item">
+                                <i class="ti-power-off text-primary"></i>
+                                Logout
+                            </a>
+                        </div>
+                    </li>
+                    {{-- <li class="nav-item nav-settings d-none d-lg-flex">
+                        <a class="nav-link" href="#">
+                            <i class="icon-ellipsis"></i>
+                        </a>
+                    </li> --}}
+                </ul>
+                <button class="navbar-toggler navbar-toggler-right d-lg-none align-self-center" type="button"
+                    data-toggle="offcanvas">
+                    <span class="icon-menu"></span>
+                </button>
+            </div>
+        </nav>
+
+
+
+        {{-- SIDEBAR SKINS --}}
+
+        <div class="container-fluid page-body-wrapper">
+            <div class="theme-setting-wrapper">
+                <div id="settings-trigger"><i class="ti-settings"></i></div>
+                <div id="theme-settings" class="settings-panel">
+                    <i class="settings-close ti-close"></i>
+                    <p class="settings-heading">SIDEBAR SKINS</p>
+                    <div class="sidebar-bg-options selected" id="sidebar-light-theme">
+                        <div class="img-ss rounded-circle bg-light border mr-3"></div>Light
+                    </div>
+                    <div class="sidebar-bg-options" id="sidebar-dark-theme">
+                        <div class="img-ss rounded-circle bg-dark border mr-3"></div>Dark
+                    </div>
+                    <p class="settings-heading mt-2">HEADER SKINS</p>
+                    <div class="color-tiles mx-0 px-4">
+                        <div class="tiles success"></div>
+                        <div class="tiles warning"></div>
+                        <div class="tiles danger"></div>
+                        <div class="tiles info"></div>
+                        <div class="tiles dark"></div>
+                        <div class="tiles default"></div>
+                    </div>
+                </div>
+            </div>
+
+
+            {{-- SIDEBAR --}}
+
+            <nav class="sidebar sidebar-offcanvas" id="sidebar">
+                <ul class="nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{url('inicio')}}">
+                            <i class="icon-grid menu-icon"></i>
+                            <span class="menu-title">INICIO</span>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" data-toggle="collapse" href="#ui-basic" aria-expanded="false"
+                            aria-controls="ui-basic">
+                            <i class="icon-layout menu-icon"></i>
+                            <span class="menu-title">USUARIOS</span>
+                            <i class="menu-arrow"></i>
+                        </a>
+                        {{-- <div class="collapse" id="ui-basic">
+                            <ul class="nav flex-column sub-menu">
+                                <li class="nav-item"> <a class="nav-link"
+                                        href="pages/ui-features/buttons.html">Buttons</a></li>
+                                <li class="nav-item"> <a class="nav-link"
+                                        href="pages/ui-features/dropdowns.html">Dropdowns</a></li>
+                                <li class="nav-item"> <a class="nav-link"
+                                        href="pages/ui-features/typography.html">Typography</a>
+                                </li>
+                            </ul>
+                        </div> --}}
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" data-toggle="collapse" href="#form-elements" aria-expanded="false"
+                            aria-controls="form-elements">
+                            <i class="icon-columns menu-icon"></i>
+                            <span class="menu-title">LOTES</span>
+                            <i class="menu-arrow"></i>
+                        </a>
+                        {{-- <div class="collapse" id="form-elements">
+                            <ul class="nav flex-column sub-menu">
+                                <li class="nav-item"><a class="nav-link" href="pages/forms/basic_elements.html">Basic
+                                        Elements</a>
+                                </li>
+                            </ul>
+                        </div> --}}
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" data-toggle="collapse" href="#charts" aria-expanded="false"
+                            aria-controls="charts">
+                            <i class="icon-bar-graph menu-icon"></i>
+                            <span class="menu-title">PARCELAS</span>
+                            <i class="menu-arrow"></i>
+                        </a>
+                        {{-- <div class="collapse" id="charts">
+                            <ul class="nav flex-column sub-menu">
+                                <li class="nav-item"> <a class="nav-link"
+                                        href="pages/charts/chartjs.html">ChartJs</a></li>
+                            </ul>
+                        </div> --}}
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" data-toggle="collapse" href="#tables" aria-expanded="false"
+                            aria-controls="tables">
+                            <i class="icon-grid-2 menu-icon"></i>
+                            <span class="menu-title">CLIENTES</span>
+                            <i class="menu-arrow"></i>
+                        </a>
+                        {{-- <div class="collapse" id="tables">
+                            <ul class="nav flex-column sub-menu">
+                                <li class="nav-item"> <a class="nav-link" href="pages/tables/basic-table.html">Basic
+                                        table</a></li>
+                            </ul>
+                        </div> --}}
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" data-toggle="collapse" href="#icons" aria-expanded="false"
+                            aria-controls="icons">
+                            <i class="icon-contract menu-icon"></i>
+                            <span class="menu-title">PRECIOS</span>
+                            <i class="menu-arrow"></i>
+                        </a>
+                        {{-- <div class="collapse" id="icons">
+                            <ul class="nav flex-column sub-menu">
+                                <li class="nav-item"> <a class="nav-link" href="pages/icons/mdi.html">Mdi icons</a>
+                                </li>
+                            </ul>
+                        </div> --}}
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" data-toggle="collapse" href="#auth" aria-expanded="false"
+                            aria-controls="auth">
+                            <i class="icon-head menu-icon"></i>
+                            <span class="menu-title">PAGOS</span>
+                            <i class="menu-arrow"></i>
+                        </a>
+                        {{-- <div class="collapse" id="auth">
+                            <ul class="nav flex-column sub-menu">
+                                <li class="nav-item"> <a class="nav-link" href="pages/samples/login.html"> Login </a>
+                                </li>
+                                <li class="nav-item"> <a class="nav-link" href="pages/samples/register.html">
+                                        Register </a></li>
+                            </ul>
+                        </div> --}}
+                    </li>
+                </ul>
+            </nav>
+
+
+
+            @yield('contenido')
+
+
+            
+
+        </div>
+
+    </div>
+
+
+    
+
+
+
+</body>
+
+</html>
