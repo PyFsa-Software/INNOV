558a68ce155ede8e2d3935d8768cea079b55aa42
danielRojas-Dev <danielrojas131415@gmail.com>
2023-07-20 15:41:40 -0300

    debug

--- resources/views/livewire/form-cobrar-cuotas.blade.php
+++ resources/views/livewire/form-cobrar-cuotas.blade.php
@@ -3,34 +3,34 @@
     @csrf
     @method('PUT')
     @if ($diferenciasDias > 0)
-    <div class="form-group">
-        <div class="alert alert-danger alert-dismissible fade show mt-2">
-            <strong>La cuota esta vencida, se debio haber abonado hace {{$diferenciasDias}} días.</strong>
+        <div class="form-group">
+            <div class="alert alert-danger alert-dismissible fade show mt-2">
+                <strong>La cuota esta vencida, se debio haber abonado hace {{ $diferenciasDias }} días.</strong>
+            </div>
         </div>
-    </div>
     @endif
     <div class="form-group">
         <label for="numero_cuota">Numero Cuota: </label>
         <input type="number" class="form-control" name="numero_cuota" id="numero_cuota"
-            value="{{old('numero_cuota', $cuota->numero_cuota)}}" disabled>
+            value="{{ old('numero_cuota', $cuota->numero_cuota) }}" disabled>
     </div>
     <div class="form-group">
         <label for="fecha_maxima_a_pagar">Fecha Maxima a Pagar: </label>
         <input type="date" class="form-control" name="fecha_maxima_a_pagar" id="fecha_maxima_a_pagar"
-            value="{{old('fecha_maxima_a_pagar', $cuota->fecha_maxima_a_pagar)}}" disabled>
+            value="{{ old('fecha_maxima_a_pagar', $cuota->fecha_maxima_a_pagar) }}" disabled>
     </div>
     <div class="form-group">
         <label for="total_estimado_a_pagar">Total Estimado a Pagar: </label>
         <input type="number" class="form-control" name="total_estimado_a_pagar" id="total_estimado_a_pagar"
-            value="{{old('total_estimado_a_pagar', $cuota->total_estimado_a_pagar)}}" disabled>
+            value="{{ old('total_estimado_a_pagar', $cuota->total_estimado_a_pagar) }}" disabled>
     </div>
 
     <div class="form-group">
         <label for="total_abonado">Formas de Pago: </label>
-        <select class="form-control" name="forma_pago" id="forma_pago" wire:model="formaPago">
-            <option value="" selected disabled>Seleccione una forma de pago</option>
+        <select class="form-control" name="forma_pago" id="forma_pago">
+            <option value="0" selected disabled>Seleccione una forma de pago</option>
             @foreach ($formasDePagos as $key => $value)
-            <option value="{{$key}}">{{$value}}</option>
+                <option value="{{ $key }}">{{ $value }}</option>
             @endforeach
         </select>
     </div>
@@ -38,7 +38,7 @@
     <div class="form-group">
         <label for="total_intereses">Interes: </label>
         <input type="number" class="form-control" name="total_intereses" id="total_intereses"
-            value="{{old('total_intereses', $totalIntereses)}}" wire:model.debounce.500ms="totalIntereses"
+            value="{{ old('total_intereses', $totalIntereses) }}" wire:model.debounce.500ms="totalIntereses"
             wire:keyup='calcularAbono'>
     </div>
 
@@ -51,7 +51,7 @@
     <div class="form-group">
         <label for="total_pago">Total a Abonar: </label>
         <input type="number" class="form-control" name="total_pago" id="total_pago"
-            value="{{old('total_pago', $totalAbonar)}}" disabled>
+            value="{{ old('total_pago', $totalAbonar) }}" disabled>
     </div>
 
     <div wire:loading>
@@ -59,7 +59,7 @@
     </div>
 
 
-    <button class="btn btn-primary mr-2 mb-2 form-control" type="button" {{$isDisabled ? 'disabled' : '' }}
+    <button class="btn btn-primary mr-2 mb-2 form-control" type="button" {{ $isDisabled ? 'disabled' : '' }}
         data-toggle="modal" data-target="#cobroCuota">Realizar
         Cobro</button>
 
