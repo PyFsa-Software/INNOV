4d4e4e4f714675295560cb36290a1d6e8c680ce2
danielRojasDev <danielrojas131415@gmail.com>
2024-08-21 10:00:38 -0300

    Se agrego el interes 0

--- app/Enums/Intereses.php
+++ app/Enums/Intereses.php
@@ -3,6 +3,7 @@
 namespace App\Enums;
 
 enum Intereses: string {
+    case INTERES = '0';
     case INTERES_BASE = '0.50';
     case INTERES_MEDIO = '1.00';
     case INTERES_ALTO = '1.50';
@@ -10,6 +11,7 @@ enum Intereses: string {
     public static function toArray(): array
     {
         return [
+            self::INTERES->name => (float) self::INTERES->value,
             self::INTERES_BASE->name => (float) self::INTERES_BASE->value,
             self::INTERES_MEDIO->name => (float) self::INTERES_MEDIO->value,
             self::INTERES_ALTO->name => (float) self::INTERES_ALTO->value,
